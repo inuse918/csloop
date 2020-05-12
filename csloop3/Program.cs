480ef45d6c42db24d5499f055ac8bf58d6cb7999
chaerin918 <s2018s34@e-mirim.hs.kr>
2020-05-12 13:36:48 +0900

    switch 조건문 사용한 화살표 입력

--- csloop3/Program.cs
+++ csloop3/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace csloop3
 {
@@ -42,6 +42,30 @@ namespace csloop3
                 Thread.Sleep(100);
                 x++;
             }
-        }
+
+            bool state = true;
+            while (state)
+            {
+                ConsoleKeyInfo info = Console.ReadKey();
+                switch (info.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("위로 이동");
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.WriteLine("오른쪽으로 이동");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine("아래쪽으로 이동");
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.WriteLine("왼쪽으로 이동");
+                        break;
+                    case ConsoleKey.X:
+                        state = false;
+                        break;
+                }
+            }
+        }//main
     }
 }
