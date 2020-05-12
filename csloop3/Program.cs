3e734075d05abf3543fb3cdac4d1f1ee89bcd89b
chaerin918 <s2018s34@e-mirim.hs.kr>
2020-05-12 13:30:39 +0900

    ToUpper, ToLower 메서드 사용 예제

--- csloop3/Program.cs
+++ csloop3/Program.cs
@@ -10,6 +10,38 @@ namespace csloop3
     {
         static void Main(string[] args)
         {
+            long start = DateTime.Now.Ticks;
+            long count = 0;
+            string input = "Potato Tomato";
+            Console.WriteLine(input.ToUpper());
+            Console.WriteLine(input.ToLower()); // 비파괴 메서드
+
+            while (start + (10000000) > DateTime.Now.Ticks)
+            {
+                count++;
+            }
+            Console.WriteLine(count + "만큼 반복함");
+            string[] array = { "사과", "배", "포도", "딸기", "바나나" };
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if (x % 3 == 0)
+                    Console.WriteLine("__@");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^_@");
+
+                Thread.Sleep(100);
+                x++;
+            }
         }
     }
 }
